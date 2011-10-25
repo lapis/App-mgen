463012587a843c1b354354ea5d1f9444c27b5a48
lapis_tw <lapis0896@gmail.com>
2011-10-26 02:25:22 +0900

    initial commit

---
+++ lib/App/mgen.pm
@@ -0,0 +1,32 @@
+package App::mgen;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::mgen -
+
+=head1 SYNOPSIS
+
+  use App::mgen;
+
+=head1 DESCRIPTION
+
+App::mgen is
+
+=head1 AUTHOR
+
+lapis_tw E<lt>lapis0896@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
